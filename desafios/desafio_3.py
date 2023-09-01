0d277dd9a078fa346c264e0577ebe30c99f46798
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-08-31 22:46:06 -0300

    docs: adicionando comentários

--- desafios/desafio_3.py
+++ desafios/desafio_3.py
@@ -1,4 +1,7 @@
-num1 = int(input('Digite um número: \n'))
-num2 = int(input('Digite outro número: \n'))
-soma = num1 + num2
-print('A soma entre', num1, 'e', num2, 'é igual a', soma)
+num1 = int(input('Digite um número: \n'))   # lê os dados de entrada...
+num2 = int(input('Digite outro número: \n'))    
+soma = num1 + num2      # efetua a soma dos números digitados
+print('A soma entre', num1, 'e', num2, 'é igual a', soma)   # exibe a resposta
+
+# o retorno da função da input() é do tipo string
+# a função int() converte uma string em number, porém um número inteiro 
