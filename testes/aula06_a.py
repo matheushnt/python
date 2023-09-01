4058d171c55c1ff30874f2bf5c430530ea341496
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-01 11:39:50 -0300

    teste realizado

---
+++ testes/aula06_a.py
@@ -0,0 +1,8 @@
+n1 = int(input('Digite um número: \n'))  # lê os dados de entrada
+n2 = int(input('Digite outro número: \n'))  # lê os dados de entrada
+soma = n1 + n2  # efetua a soma 
+print('A soma entre {} e {} é igual a {}'.format(n1, n2, soma))
+
+# a função type() exibe qual o tipo primitivo do objeto
+
+# "{}".format() exibe uma mensagem formatada dentro das chaves
