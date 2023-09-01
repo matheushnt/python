a29bda7f447313613be4a8401d0a7f589e6a2ea1
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-08-31 22:26:48 -0300

    desafio concluído

---
+++ desafios/desafio_2.py
@@ -0,0 +1,4 @@
+ano = input('Em que ano você nasceu?\n')    # lê os dados de entrada
+mês = input('Em que mês você nasceu?\n')    # lê os dados de entrada
+dia = input('Em que dia você nasceu?\n')    # lê os dados de entrada
+print('Você nasceu no dia', dia, 'de', mês, 'de', ano, ' Correto?')     # exibe a resposta
