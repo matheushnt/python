2c3e9deced83dd2cefeb4884706575765624af92
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-01 08:41:07 -0300

    exercício concluído

--- exercicios/ex002.py
+++ exercicios/ex002.py
@@ -1,2 +1,4 @@
-nome = input('Qual o seu nome?\n')
-print('Muito bem-vindo,', nome)
+nome = input('Qual o seu nome?\n')  #lê a entrada de dados
+print('Muito bem-vindo, {}'.format(nome))     # exibe a resposta
+
+# "{}"...   .format() exibe uma mensagem formatada dentro das chaves
