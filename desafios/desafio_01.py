8fc7418f6a175158b9d301f8b3a657f0da6694e3
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-01 12:41:31 -0300

    renomeando arquivos

--- desafios/desafio_01.py
+++
@@ -1,2 +0,0 @@
-nome = input('Qual o seu nome? \n')    # lê os dados de entrada
-print('Prazer em conhecê-lo(a),', nome)   # exibe o resultado
--- exercicios/ex002.py
+++ exercicios/ex002.py
@@ -1,4 +1,2 @@
-nome = input('Qual o seu nome?\n')  #lê a entrada de dados
-print('Muito bem-vindo, {}'.format(nome))     # exibe a resposta
-
-# "{}"...   .format() exibe uma mensagem formatada dentro das chaves
+nome = input('Qual o seu nome? \n')    # lê os dados de entrada
+print('Prazer em conhecê-lo(a),', nome)   # exibe o resultado
