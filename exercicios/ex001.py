7c978a972948d2f07dafe06ee89a17c6ae1ea54d
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-01 13:23:21 -0300

    renomeando e alterando arquivos

--- exercicios/ex001.py
+++ exercicios/ex001.py
@@ -1,2 +1,7 @@
-msg = 'Olá, Mundo!'
-print(msg)
+# nome = input('Qual o seu nome? \n')    # lê os dados de entrada
+# print('Prazer em conhecê-lo(a),', nome)   # exibe o resultado
+
+nome = input('Qual o seu nome?\n')  #lê a entrada de dados
+print('Muito bem-vindo, {}'.format(nome))     # exibe a resposta
+
+# "{}"...   .format() exibe uma mensagem formatada dentro das chaves
