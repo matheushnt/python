21fc1cb3abaccf54d4002e67188620e7d4bac667
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-01 12:04:01 -0300

    teste realizado

---
+++ testes/aula06_b.py
@@ -0,0 +1,10 @@
+n = input('Digite algo: ')
+print(n.isalnum())
+
+# a função bool(), se houver um valor, irá retornar verdadeiro, caso contrário, irá retornar falso
+
+# o método .isnumeric() verifica se o dado digitado é um número, se sim, retorna verdadeiro, senão, falso
+
+# o método .isalpha() verifica se o dado digitado é alfabético, se sim, retorna verdadeiro, senão, falso
+
+# o método .isalnum() verifica se o dado digitado é tanto alfanumérico, se sim, retorna verdadeiro, senão, falso
