a887c3a899d0ca611a178981476794e023b44519
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-04 17:18:10 -0300

    exercício concluído

---
+++ exercicios/ex007.py
@@ -0,0 +1,4 @@
+nota_1 = int(input('Digite sua primeira nota: '))   # lê a entrada de dados
+nota_2 = int(input('Digite sua segunda nota: '))    # lê a entrada de dados
+media = (nota_1 + nota_2) / 2   # calcula a média
+print(f'Sua média é: {media:.1f}')  # exibe a respostas
