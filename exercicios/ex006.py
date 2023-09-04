cafcf71ae3d96a6c6658f8cd6c1d5439f31c1fa5
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-04 19:29:43 -0300

    exercício concluído

--- exercicios/ex006.py
+++ exercicios/ex006.py
@@ -8,6 +8,6 @@
 #           SEGUNDA RESOLUÇÃO
 import math     # importando o módulo math
 num = int(input('Digite um número: '))      # lê a entrada de dados
-print(' Dobro: {} \n Triplo: {} \n Raiz quadrada: {:.0f}'.format(num * 2, num * 3, math.sqrt(num)))     # efetua os cálculos e exibe a resposta
+print(' Dobro: {} \n Triplo: {} \n Raiz quadrada: {:.2f}'.format(num * 2, num * 3, math.sqrt(num)))     # efetua os cálculos e exibe a resposta
 
 # a função math.sqrt() é utilizada para encontrar a raiz quadrada de um número
