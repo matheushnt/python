fe2a41219cb26d0a52cf091362546cbb4f9afcde
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-04 17:36:00 -0300

    exercício concluído

---
+++ exercicios/ex008.py
@@ -0,0 +1,4 @@
+metros = int(input('Digite um valor em METROS: '))  #lê a entrada de dados
+centimetros = metros * 100  # multiplica por 100
+milimetros = metros * 1000  # multiplica por 1000
+print('{} metros convertidos em centímetros é {} cm e convertidos em milímetros é {} mm'.format(metros , centimetros, milimetros))  # exibe a resposta
