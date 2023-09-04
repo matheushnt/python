cab262341a9484461543bda086e3df6a2c5a7de4
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-04 19:13:48 -0300

    exercício concluído

--- exercicios/ex013.py
+++ exercicios/ex013.py
@@ -1,5 +1,5 @@
-salario = float(input('Informe seu salário: '))
-aumento = salario + (salario * 0.15)
-salNovo = abs(aumento - salario)
-print(f'Seu salário terá um aumento de R$ {salNovo:.2f}')
-print(f'Com esse acréscimo de 15%, seu novo salário será: R$ {aumento:.2f}')
+salario = float(input('Informe seu salário: '))     # lê a entrada de dados
+aumento = salario + (salario * 0.15)    # calcula o aumento do salário
+salNovo = abs(aumento - salario)    # calcula de quanto será esse aumento
+print(f'Seu salário terá um aumento de R$ {salNovo:.2f}')   # exibe a respostas
+print(f'Com esse acréscimo de 15%, seu novo salário será: R$ {aumento:.2f}')    # exibe a respostas
