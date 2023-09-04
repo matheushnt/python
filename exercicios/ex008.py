4dbd917e8d17a60e0bccf29e0460eedc64a0aacb
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-04 19:52:08 -0300

    exercício concluído

--- exercicios/ex008.py
+++ exercicios/ex008.py
@@ -1,4 +1,8 @@
-metros = int(input('Digite um valor em METROS: '))  #lê a entrada de dados
-centimetros = metros * 100  # multiplica por 100
-milimetros = metros * 1000  # multiplica por 1000
-print('{} metros convertidos em centímetros é {} cm e convertidos em milímetros é {} mm'.format(metros , centimetros, milimetros))  # exibe a resposta
+metro = int(input('Digite um valor em METROS: '))  #lê a entrada de dados
+kilometro = metro / 1000
+hectometro = metro / 100
+decametro = metro / 10
+decimetro = metro * 10
+centimetro = metro * 100  # multiplica por 100
+milimetro = metro * 1000  # multiplica por 1000
+print(f'KM: {kilometro} \nHM: {hectometro} \nDAM: {decametro} \nM: {metro} \nDM: {decimetro} \nCM: {centimetro} \nMM: {milimetro}')  # exibe a resposta
