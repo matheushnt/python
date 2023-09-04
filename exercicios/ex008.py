83a20b049103add1976b564b92010463420678ea
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-04 19:55:19 -0300

    docs: explicando melhor os cálculos

--- exercicios/ex008.py
+++ exercicios/ex008.py
@@ -1,8 +1,8 @@
 metro = int(input('Digite um valor em METROS: '))  #lê a entrada de dados
-kilometro = metro / 1000
-hectometro = metro / 100
-decametro = metro / 10
-decimetro = metro * 10
+kilometro = metro / 1000  # divide por 1000
+hectometro = metro / 100  # divide por 100
+decametro = metro / 10    # divide por 10
+decimetro = metro * 10    # multiplica por 10
 centimetro = metro * 100  # multiplica por 100
 milimetro = metro * 1000  # multiplica por 1000
 print(f'KM: {kilometro} \nHM: {hectometro} \nDAM: {decametro} \nM: {metro} \nDM: {decimetro} \nCM: {centimetro} \nMM: {milimetro}')  # exibe a resposta
