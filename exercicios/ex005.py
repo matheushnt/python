64b8e32c9e1ebedac30efdf45a3f543b3c99e21e
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-04 16:19:55 -0300

    docs: explicando a lógica

--- exercicios/ex005.py
+++ exercicios/ex005.py
@@ -1,3 +1,3 @@
-num = int(input('Digite um número: '))
+num = int(input('Digite um número: '))      # lê a entrada de dados
 # print(' Número digitado: {} \n ANTECESSOR: {} \n SUCESSOR: {}'.format(num, num - 1, num + 1))
-print(f' Número digitado: {num} \n ANTECESSOR: {num - 1} \n SUCESSOR: {num + 1}')
+print(f' Número digitado: {num} \n ANTECESSOR: {num - 1} \n SUCESSOR: {num + 1}')       # exibe a resposta
