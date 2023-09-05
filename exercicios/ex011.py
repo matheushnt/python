0b6226e8948a41fa416e72266251e71328cc4820
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-05 14:01:14 -0300

    exercício concluído

--- exercicios/ex011.py
+++ exercicios/ex011.py
@@ -1,6 +1,6 @@
-comprimento = float(input('Qual o comprimento da parede? \n'))
-altura = float(input('Qual a altura da parede? \n'))
-tinta = 4
-area = comprimento * altura
-print(f'A área da parede é: {area}')
-print(f'Com 1L de tinta, é possível pintar {area % tinta} m quadrados')
+comprimento = float(input('Qual o comprimento da parede? \n'))      # lê a entrada de dados
+altura = float(input('Qual a altura da parede? \n'))        # lê a entrada de dados
+area = comprimento * altura     # calcula a área da parede
+tinta = area / 2    # calcula a quantidade de tinta necessária para pintar a parede levando em consideração que, a cada d 2m², utiliza-se 1L de tinta
+print(f'A área da parede é: {area:.2f}')    # exibe a resposta
+print(f'Para pintar essa parede, você precisará de {tinta:.3f} litros')     # exibe a resposta
