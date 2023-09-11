d1ce6854fe54644ecf96419af5d2251bf345ed9b
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-11 16:50:02 -0300

    exercício concluído

---
+++ exercicios/ex016.py
@@ -0,0 +1,3 @@
+from math import trunc      # importando somente a função trunc()
+num = float(input('Digite um número qualquer (EX.: 3.45): '))
+print(f'O número {num} possui como parte inteira {trunc(num)}')
