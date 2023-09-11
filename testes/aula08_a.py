e97351eb393da1736bc44893b9a89b0d5ee46ca1
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-11 15:36:27 -0300

    teste realizado

---
+++ testes/aula08_a.py
@@ -0,0 +1,4 @@
+import math     # importando a biblioteca math
+num = int(input('Digite um número: '))
+raiz = math.sqrt(num)   # utilizando a função math.sqrt() para encontrar a raiz quadrada
+print(f'A raiz de {num} é igual a {raiz:.3f}')
