816b7e6fa304d95d16274476446a4943e3a5c261
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-13 16:03:24 -0300

    exercício concluído

--- exercicios/ex020.py
+++ exercicios/ex020.py
@@ -1,7 +1,9 @@
-from random import sample
+from random import shuffle
 al1 = str(input('Qual o nome do primeiro aluno: '))
 al2 = str(input('Qual o nome do segundo aluno: '))
 al3 = str(input('Qual o nome do terceiro aluno: '))
 al4 = str(input('Qual o nome do quarto aluno: '))
 alunos = [al1, al2, al3, al4]
-print(f'O seguintes alunos irão se apresentar nessa ordem: \n{al1} será o {sample(4)}° \n{al2} será o {sample(4)}° \n{al3} será o {sample(4)}° \n{al4} será o {sample(4)}°')
+shuffle(alunos)
+print('A ordem de apresentação será: ')
+print(alunos)
