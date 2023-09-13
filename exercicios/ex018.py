37dc971ec2dd2603da5d335e507bff8627e6d923
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-13 15:21:43 -0300

    exercício corrigido

--- exercicios/ex018.py
+++ exercicios/ex018.py
@@ -1,3 +1,5 @@
-from math import cos, sin       # importando as funções de cosseno e seno
+from math import cos, sin, tan, radians       # importando as funções de cosseno, seno, tangente e radianos
 ang = float(input('Informe um ângulo qualquer: '))
-print(f'O cosseno de {ang} é igual a {cos(ang):.4f} e o seno é equivalente a {sin(ang):.4f}')
+print(f'O Seno é igual a {sin(radians(ang)):.2f}')      # primeiramente converte o ângulo para radianos para então realizar o cálculo do Seno
+print(f'O Cosseno é igual a {cos(radians(ang)):.2f}')       # primeiramente converte o ângulo para radianos para então realizar o cálculo do Cosseno
+print(f'A Tangente é igual a {tan(radians(ang)):.2f}')      # primeiramente converte o ângulo para radianos para então realizar o cálculo da Tangente
