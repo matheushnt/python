63b09dbde4ca1d775a96cd1ab19111bc0c0fa178
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-13 16:47:14 -0300

    exercício não concluído por causa da lib pygame

---
+++ exercicios/ex021.py
@@ -0,0 +1,10 @@
+import pygame
+pygame.init()
+pygame.mixer.music.load('ex021.mp3')
+pygame.mixer.music.play()
+input()
+pygame.event.wait()
+
+# não tocou a música que deveria ser tocada...
+
+# por conta da inconveniência, desinstalei a lib pygame
