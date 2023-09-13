ae744bf3fd019827c2757eb1c4317808a4619efd
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-13 14:50:17 -0300

    docs: realizando o cálculo da hipotenusa sem o uso de funções

--- exercicios/ex017.py
+++ exercicios/ex017.py
@@ -3,3 +3,4 @@ co = float(input('Informe o comprimento do Cateto Oposto: '))
 ca = float(input('Informe o comprimento do Cateto Adjacente: '))
 hip = hypot(co, ca)     # o método hypot realiza o cálculo do comprimento da hipotenusa
 print(f'O comprimento da Hipotenusa mede: {hip:.3f} ')
+# hip = (co ** 2) + (ca ** 2) ** 1/2
