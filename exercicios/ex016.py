a3b723883a3384121e5e62e95a5b76df253f2c33
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-13 14:11:33 -0300

    docs: explicando o uso da função trunc()

--- exercicios/ex016.py
+++ exercicios/ex016.py
@@ -1,3 +1,3 @@
 from math import trunc      # importando somente a função trunc()
 num = float(input('Digite um número qualquer (EX.: 3.45): '))
-print(f'O número {num} possui como parte inteira {trunc(num)}')
+print(f'O número {num} possui como parte inteira {trunc(num)}')     # o método trunc() retorna somente a parte inteira de um número real
