63202cf989581bb7f9ba51ab4f5454b8f59e6a8b
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-13 16:08:39 -0300

    docs: explicando o uso da função shuffle()

--- exercicios/ex020.py
+++ exercicios/ex020.py
@@ -4,6 +4,6 @@ al2 = str(input('Qual o nome do segundo aluno: '))
 al3 = str(input('Qual o nome do terceiro aluno: '))
 al4 = str(input('Qual o nome do quarto aluno: '))
 alunos = [al1, al2, al3, al4]
-shuffle(alunos)
+shuffle(alunos)     # A função shuffle() é utilizada para exibir o resultado de uma lista ou de uma tupla de forma embaralhada
 print('A ordem de apresentação será: ')
-print(alunos)
+print(alunos)       # será exibido a lista alunos de forma embaralhada
