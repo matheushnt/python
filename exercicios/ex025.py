47d67d4827748bdc96f4ccb552746832b4ef01cd
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-14 18:27:58 -0300

    exercício concluído

---
+++ exercicios/ex025.py
@@ -0,0 +1,2 @@
+nome = str(input('Digite seu nome completo: '))
+print(f'Você possui o nome SILVA. Resposta: {"Silva" in nome}')         # verifica se há o nome SILVA, se sim, retorna True, senão, False
