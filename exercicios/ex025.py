13f1b221b013e2279933656661a3b22c4501eda6
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-15 14:31:18 -0300

    docs: explicando a lógica

--- exercicios/ex025.py
+++ exercicios/ex025.py
@@ -1,2 +1,2 @@
 nome = str(input('Digite seu nome completo: '))
-print(f'Você possui o nome SILVA. Resposta: {"silva" in nome.lower}')         
+print(f'Você possui o nome SILVA. Resposta: {"silva" in nome.lower}')         # verifica se há o nome SILVA, se sim, retorna True, senão, False
