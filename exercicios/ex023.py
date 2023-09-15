f48ef4a61fe1c0f5b0b90729eb13e9b30646a3f3
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-15 14:09:45 -0300

    exercício concluído

--- exercicios/ex023.py
+++ exercicios/ex023.py
@@ -1,6 +1,9 @@
-num = str(input('Digite um número: '))
-dividido = num.split()
-print(f'Unidade: {dividido[0][3]}')
-print(f'Dezena: {dividido[0][2]}')
-print(f'Centena: {dividido[0][1]}')
-print(f'Milhar: {dividido[0][0]}')
+num = int(input('Digite um número: '))
+un = num // 1 % 10              
+dez = num // 10 % 10            
+cent = num // 100 % 10               
+milhar = num // 1000 % 10       
+print(f'Unidade: {un}')
+print(f'Dezena: {dez}')
+print(f'Centena: {cent}')
+print(f'Milhar: {milhar}')
