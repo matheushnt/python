d32ad52501a6b0744ade0c3e49c88b1578a1fd93
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-15 14:10:23 -0300

    docs: explicando a lógica da obtenção dos valores

--- exercicios/ex023.py
+++ exercicios/ex023.py
@@ -1,8 +1,8 @@
 num = int(input('Digite um número: '))
-un = num // 1 % 10              
-dez = num // 10 % 10            
-cent = num // 100 % 10               
-milhar = num // 1000 % 10       
+un = num // 1 % 10              # resulta na unidade do valor digitado
+dez = num // 10 % 10            # resulta na dezena do valor digitado
+cent = num // 100 % 10          # resulta na centena do valor digitado     
+milhar = num // 1000 % 10       # resulta no milhar do valor digitado
 print(f'Unidade: {un}')
 print(f'Dezena: {dez}')
 print(f'Centena: {cent}')
