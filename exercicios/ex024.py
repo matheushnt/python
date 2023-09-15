fb21c036e198d3cae594c4c724ec7e9bf98c698c
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-15 14:23:13 -0300

    docs: explicando a lógica

--- exercicios/ex024.py
+++ exercicios/ex024.py
@@ -1,2 +1,2 @@
 nome = str(input('Digite o nome de uma cidade: ')).strip()
-print(f'A cidade começa com o nome SANTO. Resposta: {nome[:5].upper() == "SANTO"}')     
+print(f'A cidade começa com o nome SANTO. Resposta: {nome[:5].upper() == "SANTO"}')     # exibe True se SANTO for o primeiro nome da cidade digitada, caso contrário, retorna False
