b8d88af5bee6948140d5e46f0157ddf1756c8ba8
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-19 18:57:58 -0300

    docs: inserindo informação sobre a multa

--- exercicios/ex029.py
+++ exercicios/ex029.py
@@ -1,7 +1,7 @@
 velocidade = int(input('Informe a velocidade que você estava trafegando na pista:\n'))
 if velocidade > 80:
     print(f'Aviso de Multa por Excesso de Velocidade. Você estava acima de 80Km/h.')
-    multa = (velocidade - 80) * 7
+    multa = (velocidade - 80) * 7       # calcula a multa
     print(f'A multa será no valor de R$ {multa:.2f}')
 else:
     print('Parabéns pela sua condução segura e por fazer a diferença nas estradas.')
