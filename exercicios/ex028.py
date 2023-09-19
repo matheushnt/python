01e685e077f92d54b93ca7a5ac61ad0d6b8272ac
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-19 18:33:53 -0300

    docs: explicando o uso da função randint() e o result. do programa

--- exercicios/ex028.py
+++ exercicios/ex028.py
@@ -1,8 +1,9 @@
-from random import randint 
-numAleatorio = randint(0, 5)
+from random import randint      # nesse exercício, o método randint() atende melhor os requisitos
+numAleatorio = randint(0, 5)        # retorna um valor inteiro entre 0 e 5
 chute = int(input('Pensei em um número, tente acertá-lo.\n'))
 print(f'O número que eu pensei foi: {numAleatorio}')
 if chute == numAleatorio:
     print(f'PARABÉNS. Você acertou.')
 else:
     print(f'Infelizmente você errou.')
+# exibe a resposta de acordo com o resultado da comparação
