beb0cd99cd93cdd5c62b4d1e003b0a052a726e32
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-20 19:06:38 -0300

    feat: adicionando o método sleep() e date.today()

--- exercicios/ex032.py
+++ exercicios/ex032.py
@@ -1,6 +1,13 @@
+from time import sleep
+from datetime import date
 print('--- Ano Bissexto ---')
+print('Caso digite 0, será analisado o ano atual.')
 ano = int(input('Digite um ano: '))
-if ano % 4 == 0:        # verifica se o ano é bissexto ou não
+print('PROCESSANDO...')
+if ano == 0:
+    ano = date.today().year     # obtem a data atual, porém somente o ano nesse caso
+sleep(2)
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:        # verifica se o ano é bissexto ou não
     print(f'{ano} é um ano bissexto.')
 else:
     print(f'{ano} não é um ano bissexto.')
