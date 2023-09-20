9d81379e8596e26c8bd79166fc8661f08e5be8d0
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-09-20 14:40:57 -0300

    feat: adicionando mensagens de início e fim

--- exercicios/ex031.py
+++ exercicios/ex031.py
@@ -1,3 +1,4 @@
+print('--- Análise de Distância ---')
 distancia = int(input('Qual a distância em KM entre o início e o seu destino: '))
 if distancia <= 200:
     custo = distancia * 0.5
@@ -5,3 +6,4 @@ if distancia <= 200:
 else:
     custo = distancia * 0.45
     print(f'Sua viagem custará R$ {custo:.2f}')
+print('-------- FIM --------')
