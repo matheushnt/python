a090de58bf1a8fe42cbdecdff32905af3e956d57
Matheus <matheushnt06@gmail.com>
2023-09-26 18:42:16 -0300

    exercício concluído

---
+++ exercicios/ex039.py
@@ -0,0 +1,16 @@
+print('\033[0;32m****** ALISTAMENTO MILITAR ******\033[m')
+from time import sleep
+from datetime import date
+anoAtual = date.today().year        # obtem o ano atual de acordo com o Sistema Operacional
+dataDeNascimento = int(input('Informe o seu ano de nascimento: '))
+idade = anoAtual - dataDeNascimento
+alistamento = 18
+print('PROCESSANDO...')
+sleep(2)
+if idade < alistamento: 
+    print(f'Você ainda tem {idade} anos. Aguarde até completar 18 anos para comparecer a uma Junta Militar para realizar seu Alistamento Militar.')
+elif idade == alistamento:
+    print(f'Você tem {idade} anos. Compareça a Junta Militar mais próxima de você para realizar seu Alistamento Militar.')
+else:
+    print(f'Você tem {idade} anos. Compareça o mais breve possível a uma Junta Militar para realizar seu Alistamento Militar.')
+print('\033[0;36m****** FIM ******\033[m')
