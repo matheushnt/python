0fda4e3ca5fada8e9a08cb41881cf55c4477c9c9
Matheus <matheushnt06@gmail.com>
2023-09-27 18:52:01 -0300

    feat: alterei a forma do cálculo

--- exercicios/ex036.py
+++ exercicios/ex036.py
@@ -4,10 +4,10 @@ casa = float(input('Qual o valor da casa?\n R$ '))
 salario = float(input('Quanto você ganha de salário?\n R$ '))
 anos = int(input('Em quantos anos você ficará pagando a casa?\n'))
 mensalidade = casa / (anos * 12)        # calcula o preço da mensalidade 
-excesso = salario + (salario * 30 / 100)        # calcula o excesso. Acrescenta 30% a mais no salário.
+minimo = salario * 30 / 100       # calcula 30% do salário
 print('CALCULANDO...')
 sleep(2)
-if mensalidade > excesso:       # Caso a mensalidade seja maior do que o salário acrescido com 30%, empréstimo negativado
+if mensalidade > minimo:       # Caso a mensalidade seja maior do que 30% do salário, empréstimo negativado
     print(f'A mensalidade será R$ {mensalidade:.2f}')
     print('Infelizmente você não poderá fazer o empréstimo. A mensalidade não pode exceder 30' + "%" ' do seu salário')
 else:       # empréstimo aprovado
