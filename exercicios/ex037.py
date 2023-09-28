9555d365aa5687825124b86db8338c86d0bd4b70
Matheus <matheushnt06@gmail.com>
2023-09-28 13:41:45 -0300

    feat: alterei as mensagens de exibição

--- exercicios/ex037.py
+++ exercicios/ex037.py
@@ -7,11 +7,13 @@ print('PROCESSANDO...')
 sleep(2)
 if base == 1:
     conversao = bin(num)[2:]        # converte para base binária
+    print(f'O número {num} convertido para a base Binária é {conversao}')
 elif base == 2:
     conversao = oct(num)[2:]        # converte para base octal
+    print(f'O número {num} convertido para a base Octal é {conversao}')
 elif base == 3:
     conversao = hex(num)[2:]        # converte para base hexadecimal
+    print(f'O número {num} convertido para a base Hexadecimal é {conversao}')
 else:
     print('Número inválido')
-print(f'O número {num} convertido para a base {base} é {conversao}')
 print('\033[0;31m------ FIM ------\033[m')
