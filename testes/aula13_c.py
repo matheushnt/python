d59010fa07f6c1ef4121ad094dfaf8c5ef76177c
Matheus <matheushnt06@gmail.com>
2023-10-02 13:40:07 -0300

    teste realizado

---
+++ testes/aula13_c.py
@@ -0,0 +1,5 @@
+soma = 0 
+for contador in range(0, 3):
+    num = int(input('Digite um valor: '))
+    soma += num         # soma = soma + num
+print(f'Soma entre os valores digitados é igual a: {soma}')
