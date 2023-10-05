f18234b16e234e77d12339f668e87149d68deede
Matheus <matheushnt06@gmail.com>
2023-10-05 15:45:49 -0300

    exercício concluído

---
+++ exercicios/ex053.py
@@ -0,0 +1,9 @@
+print('\033[1;30;107m------ Detector de Palíndromo ------\033[m')
+frase = str(input('Digite uma frase:\n'))
+fraseTratada = frase.lower().replace(' ', '')       # transforma tudo para minúsculo e retira os espaços
+fraseInvertida = fraseTratada[::-1]         # inverte a ordem da string usando a indexação negativa
+if fraseTratada == fraseInvertida:
+    print('É um Palíndromo')
+else:
+    print('Não é um Palíndromo.')
+print('\033[1;30;107m------ FIM ------\033[m')
