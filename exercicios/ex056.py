d87ef8f7d7c4001c3a67d4f04752228456343c99
Matheus <matheushnt06@gmail.com>
2023-10-06 14:28:42 -0300

    exercício concluído

--- exercicios/ex056.py
+++ exercicios/ex056.py
@@ -1,15 +1,18 @@
 from statistics import mean
+from time import sleep
+print('\033[1;30;107m------ ANALISADOR ------\033[m')
 idades = []
 nomes = []
-for pessoa in range(1, 3):
-    nome = str(input('Qual o seu nome?\n'))
-    nomes.append(nome)
+for pessoa in range(1, 4):
+    nome = str(input('Qual o seu nome?\n').capitalize())
+    nomes.append(nome)      # adiciona o nome digitado dentro da lista nomes
     idade = int(input('Qual sua idade?\n'))
-    idades.append(idade)
-    sexo = str(input('Qual seu sexo?\n')).lower().strip()
+    idades.append(idade)    # adiciona a idade digitada dentro da lista idades
     print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
 mediaDeIdade = mean(idades)     # o método mean() calcula a média aritmética de uma lista de números
 maiorIdade = max(idades)        # obtém o maior valor da lista idades
-homemMaisVelho = idades.index(maiorIdade)       # obtém o INDEX do maior valor da lista idades
+pessoaMaisVelha = idades.index(maiorIdade)       # obtém o ÍNDICE da maior idade da lista idades
+sleep(1)
 print(f'A média da idades dos indivíduos é igual a {mediaDeIdade:.2f}')
-print(f'{nomes[homemMaisVelho]} é a pessoa mais velha com {maiorIdade} anos')
+print(f'{nomes[pessoaMaisVelha]} é a pessoa mais velha com {maiorIdade} anos')
+print('\033[1;30;107m------ FIM ------\033[m')
