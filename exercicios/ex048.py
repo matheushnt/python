f52a08e251bf6512efb230f508fed1efdb08f5b2
Matheus <matheushnt06@gmail.com>
2023-10-09 14:28:35 -0300

    docs: analisando o acumulador

--- exercicios/ex048.py
+++ exercicios/ex048.py
@@ -1,11 +1,11 @@
 from time import sleep
 print('\033[4;35m=-=-=- SOMA DE ÍMPARES E MÚLTIPLOS DE TRÊS -=-=-=\033[m')
-soma = 0
+soma = 0    # acumulador 
 for contador in range(1, 501, 2):
     # verifica se o número é ímpar e múltiplo de três
     if contador % 3 == 0:
         print(f'O número {contador} é ímpar e múltiplo de três.')
-        soma += contador
+        soma += contador    # acumulador
         sleep(0.05)
 print(f'A soma entre todos os números ímpares múltiplos de 3 é igual a {soma}')
 print('\033[4;35m=-=-=- FIM -=-=-=\033[m')
