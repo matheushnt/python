467c60640781b545bfdb9c2ff3cebde88442cdc4
Matheus <matheushnt06@gmail.com>
2023-10-10 19:44:03 -0300

    teste realizado

---
+++ testes/aula14_a.py
@@ -0,0 +1,9 @@
+"""for contador in range(1, 11):
+    print(contador)
+print('FIM')"""
+
+contador = 1    # inicialização
+while contador <= 10:   # teste lógico
+    print(contador)
+    contador += 1   # incremento
+print('FIM')
