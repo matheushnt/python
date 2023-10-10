720940c833d164d7e33498ff28f8b3ae0800a7b3
Matheus <matheushnt06@gmail.com>
2023-10-10 15:11:24 -0300

    feat: adicionando a função print() para verificar se é um palíndromo

--- exercicios/ex053.py
+++ exercicios/ex053.py
@@ -2,6 +2,7 @@ print('\033[1;30;107m------ Detector de Palíndromo ------\033[m')
 frase = str(input('Digite uma frase:\n'))
 fraseTratada = frase.lower().replace(' ', '')       # transforma tudo para minúsculo e retira os espaços
 fraseInvertida = fraseTratada[::-1]         # inverte a ordem da string usando a indexação negativa
+print(f'{fraseTratada} é {fraseInvertida}')
 if fraseTratada == fraseInvertida:
     print('É um Palíndromo')
 else:
