1aa21dc14af4462af1b028c348b1421b94c247c3
Matheus <matheushnt06@gmail.com>
2023-10-10 19:56:40 -0300

    teste realizado

---
+++ testes/aula14_b.py
@@ -0,0 +1,10 @@
+# Neste caso eu sei o limite de iterações
+"""for contador in range(1, 6):
+    num = int(input('Digite um número: '))
+print('FIM')"""
+
+# Neste caso eu não sei o limite de iterações
+num = 1    # inicialização
+while num != 0:    # condição de parada
+    num = int(input('Digite um número: '))
+print('FIM')
