0dde95a0a2a91c4782ce4d5f1cf6fc472c9ebde9
Matheus <matheushnt06@gmail.com>
2023-10-11 14:50:57 -0300

    exercício incompleto

---
+++ exercicios/ex057.py
@@ -0,0 +1,4 @@
+contador = 1
+sexoInformado = str(input('Informe o seu sexo: ')).upper()
+while sexoInformado != 'M' or sexoInformado != 'F':
+    sexoInformado = str(input('Dados inválidos. Por favor, informe novamente: '))
