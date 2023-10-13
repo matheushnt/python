13e39c4fac928cb10de7481d644eac08d5e449e3
Matheus <matheushnt06@gmail.com>
2023-10-13 13:46:47 -0300

    feat: remoção de espaços inúteis na função input()

--- exercicios/ex057.py
+++ exercicios/ex057.py
@@ -1,8 +1,8 @@
 print('\033[1;33m=-=-=- VALIDAÇÃO DE DADOS -=-=-=\033[m')
-sexo = str(input('Ingorme o seu sexo [M/F]: ')).upper()
+sexo = str(input('Ingorme o seu sexo [M/F]: ')).upper().strip()
 if sexo != 'M' and sexo != 'F':     # se o sexo informado for diferente do solicitado, executa o laço 
     while sexo != 'M' and sexo != 'F':      # enquanto o dado informado for diferente do solicitado, executa o laço
-        sexo = str(input('Dados inválidos. Por favor, informe novamente seu sexo: ')).upper()
+        sexo = str(input('Dados inválidos. Por favor, informe novamente seu sexo: ')).upper().strip()
         if sexo == 'M' or sexo == 'F':      # se o sexo informado for igual ao solicitado, termina o laço
             print(f'Sexo {sexo} cadastrado com sucesso.')
 else:   # se sexo informado for igual ao solicitado, cadastrado é realizado com sucesso
