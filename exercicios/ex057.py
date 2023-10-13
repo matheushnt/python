09ecca8446657d863e6af67c0b0b29fc19eb57e4
Matheus <matheushnt06@gmail.com>
2023-10-13 14:02:43 -0300

    feat: fatiando os dados informados

--- exercicios/ex057.py
+++ exercicios/ex057.py
@@ -1,8 +1,8 @@
 print('\033[1;33m=-=-=- VALIDAÇÃO DE DADOS -=-=-=\033[m')
-sexo = str(input('Ingorme o seu sexo [M/F]: ')).upper().strip()
+sexo = str(input('Informe o seu sexo [M/F]: ')).upper().strip()[0]
 if sexo != 'M' and sexo != 'F':     # se o sexo informado for diferente do solicitado, executa o laço 
-    while sexo != 'M' and sexo != 'F':      # enquanto o dado informado for diferente do solicitado, executa o laço
-        sexo = str(input('Dados inválidos. Por favor, informe novamente seu sexo: ')).upper().strip()
+    while sexo not in 'MmFf':      # enquanto o dado informado for diferente do solicitado, executa o laço
+        sexo = str(input('Dados inválidos. Por favor, informe novamente seu sexo: ')).upper().strip()[0]
         if sexo == 'M' or sexo == 'F':      # se o sexo informado for igual ao solicitado, termina o laço
             print(f'Sexo {sexo} cadastrado com sucesso.')
 else:   # se sexo informado for igual ao solicitado, cadastrado é realizado com sucesso
