a624a8e3c23b59c730674cc26c400d76f65e6821
Matheus <matheushnt06@gmail.com>
2023-10-19 16:29:05 -0300

    exercício concluído

--- exercicios/ex062.py
+++ exercicios/ex062.py
@@ -2,7 +2,14 @@ primeiro = int(input('Primeiro termo: '))
 razao = int(input('Razão da PA: '))
 termo = primeiro
 contador = 1
-while contador <= 10:
-    print(f'{termo} ➞ 'if contador <= 10 else 'PAUSA', end=' ')
-    termo += razao
-    contador += 1
+total = 0   # esse total será calculado ainda
+mais = 10
+while mais != 0:
+    total += mais   # soma a quantidade de termos solicitados
+    while contador <= total:
+        print(f'{termo} ➞ ', end=' ')
+        termo += razao
+        contador += 1
+    print('PAUSA')
+    mais = int(input('Quantos termos você quer mostrar a mais?\n'))
+print(f'Progressão finalizada com {total} termos.')
