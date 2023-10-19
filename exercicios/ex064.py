8be95686c6e3f75a0bcf930d7a71923e85952b5f
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-10-19 20:02:39 -0300

    refactor: lógica refatorada

--- exercicios/ex064.py
+++ exercicios/ex064.py
@@ -4,9 +4,10 @@ print('=-' * 30)
 numeros = 0
 soma = 0
 num = 0     # inicialização
+num = int(input('Digite um número (999 para PARAR: '))
 while num != 999:   # teste lógico
-    num = int(input('Digite um número (999 para PARAR): '))
     soma += num     # soma os números digitados
     numeros += 1    # incremento
+    num = int(input('Digite um número (999 para PARAR): '))
 print('=-' * 30)
-print(f'Programa finalizado com {numeros - 1} números. Somando todos resultam em {soma - 999}')
+print(f'Programa finalizado com {numeros} números. Somando todos resultam em {soma}')
