224c3b9a5bc59e4c21aa50d5e83609c02cf8dbee
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-10-20 14:49:12 -0300

    exercício concluído

--- exercicios/ex065.py
+++ exercicios/ex065.py
@@ -1,13 +1,46 @@
+from statistics import mean
 numeros = []
-soma = 0
 num = int(input('Digite um número: '))
 numeros.append(num)
-resp = str(input('Quer continuar? [SIM/NÃO]: ')).strip()
+resp = str(input('Quer continuar? [SIM/NÃO]: ')).strip()[0]
 if resp in 'SIMsim':
-    while resp in 'SIMsim':
+    while resp in 'Ss':
         num = int(input('Digite um número: '))
-        soma += num
         numeros.append(num)
-        resp = str(input('Quer continuar? [SIM/NÃO]: ')).strip()
-
-''' Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e qual foi o maior e menor valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores. '''
+        resp = str(input('Quer continuar? [SIM/NÃO]: ')).strip()[0]
+    media = mean(numeros)
+    print('~' * 30)
+    print(f'{'INFORMAÇÕES IMPORTANTES':^30}')
+    print('~' * 30)
+    print(f'O maior número digitado: {max(numeros)}')
+    print('-' * 30)
+    print(f'O menor número digitado: {min(numeros)}')
+    print('-' * 30)
+    print(f'A Média Aritmética: {media:.2f}')
+    print('-' * 30)
+    print(f'{'FIM':^30}')
+    print('-' * 30)
+else:   # se o usuário digitar diferente de zero, é exibido uma perguntar 
+    print('Há poucos dados informados. Não há como fazer uma análise.')
+    resp = str(input('Você gostaria de inserir mais dados?\n'))[0]
+    if resp in 'Ss':
+        while resp in 'SIMsim':
+            num = int(input('Digite um número: '))
+            numeros.append(num)
+            resp = str(input('Quer continuar? [SIM/NÃO]: ')).strip()[0]
+        media = mean(numeros)
+        print('~' * 30)
+        print(f'{'INFORMAÇÕES IMPORTANTES':^30}')
+        print('~' * 30)
+        print(f'O maior número digitado: {max(numeros)}')
+        print('-' * 30)
+        print(f'O menor número digitado: {min(numeros)}')
+        print('-' * 30)
+        print(f'A Média Aritmética: {media:.2f}')
+        print('-' * 30)
+        print(f'{'FIM':^30}')
+        print('-' * 30)
+    else:
+        print('-' * 30)
+        print('Tudo bem! Volte sempre.')
+        print('-' * 30)
