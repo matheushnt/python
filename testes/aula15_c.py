58158328173ff3eddd6183a87021cc8656cf6707
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-10-24 13:32:09 -0300

    teste realizado

---
+++ testes/aula15_c.py
@@ -0,0 +1,8 @@
+# loop infinito com o comando break para interromper o laço
+n = s = 0
+while True:
+    n = int(input('Digite um número: '))
+    if n == 999:
+        break
+    s += n
+print(f'A soma vale {s}')
