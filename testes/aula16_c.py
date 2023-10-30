1b70a7c7baaa11eeb4dc274cae29c870a3493148
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-10-30 14:46:15 -0300

    teste realizado

---
+++ testes/aula16_c.py
@@ -0,0 +1,5 @@
+# o método sorted() organiza a tupla de forma alfabética e crescente
+lanche = ('hambúrguer', 'lasanha', 'pizza', 'pudim')
+print(sorted(lanche))
+números = (15, 29, 13, 1, 2, 45.5, 100, 89)
+print(sorted(números))
