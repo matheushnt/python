10ed22494aff71ede01472f3b74ea7faccd128dd
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-11-05 19:48:59 -0300

    exercício concluído

--- exercicios/ex076.py
+++ exercicios/ex076.py
@@ -1,10 +1,12 @@
-produtos_precos = ('Lápis', 1.75, 'Borracha', 2.00, 'Caderno', 15.90,
-                   'Estojo', 25.00, 'Transferidor', 4.20, 'Compasso', 9.99,
-                   'Mochila', 120.32, 'Canetas', 22.30, 'Livro', 34.90)
+produtos = ('Lápis', 1.75, 'Borracha', 2.00, 'Caderno', 15.90,
+            'Estojo', 25.00, 'Transferidor', 4.20, 'Compasso', 9.99,
+            'Mochila', 120.32, 'Canetas', 22.30, 'Livro', 34.90)
 print('-' * 60)
 print(f'{'LISTAGEM DE PREÇOS':^60}')
 print('-' * 60)
-for produtos in range(0, len(produtos_precos), 2):
-    print(f'{produtos_precos[produtos]}')
-for precos in range(1, len(produtos_precos), 2):
-    print(f'{produtos_precos[precos]}')
+for posicao in range(0, len(produtos)):
+    if posicao % 2 == 0:
+        print(f'{produtos[posicao]:.<50}', end='')
+    else:
+        print(f'R$ {produtos[posicao]:>6.2f}')
+print('=' * 60)
