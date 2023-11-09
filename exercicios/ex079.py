138fc9106f8ec42181c08f550bcc4d00bb62319c
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-11-09 20:29:47 -0300

    exercício concluído

--- exercicios/ex079.py
+++ exercicios/ex079.py
@@ -1,16 +1,20 @@
-numeros = []
-while True:
-    num = int(input('Digite um número: '))
-    if num in numeros:
-        num = int(input('Número já informado. Digite outro: '))
-        while num in numeros:
-            num = int(input('Número já informado. Digite outro: '))
-    else:
-        numeros.append(num)
-    resp = str(input('Quer continuar? [S/N]\n'))[0].strip()
-    if resp in 'Nn':
-        break
-print(f'Os números informados foram: {num}', end=' ')
-num_organizados = sorted(numeros)
-for n in num_organizados:
-    print(f'{n}', end=' ')
+from time import sleep
+valores = []
+print('-'*30)
+print(f'{'VALORES ÚNICOS EM UMA LISTA':^30}')
+print('-'*30)
+while True: # loop infinito
+    valor = int(input('Digite um número: '))
+    if valor not in valores:    # se o valor digitado não estiver em na lista valores, será adicionando na lista
+        valores.append(valor)   # valor adicionado
+    else:   # não será adicionado 
+        print('Valor já adicionado!')
+    print('-'*30)
+    resposta = str(input('Quer continuar? [S/N]\n'))[0]     # pergunta se o usuário quer continuar a adicionar números
+    while resposta not in 'SsNn':   # se a resposta for diferente de S/N, dispará o loop
+        resposta = str(input('Quer continuar? [S/N]\n'))[0]
+    if resposta in 'Nn':    # se a resposta a pergunta for negativa, o usuário irá parar de adicionar números
+        break   # interrompe o loop infinito
+print('PROCESSANDO...')
+sleep(2)
+print(f'Valores adicionados: {sorted(valores)}')    # exibe a lista organizada de forma crescente
