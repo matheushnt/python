424c8fbd3d6825d2dcc10ba4bd96cac62f7f4a90
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-11-10 15:25:54 -0300

    exercício concluído

---
+++ exercicios/ex083.py
@@ -0,0 +1,13 @@
+expressao = str(input('Digite uma expressão: '))
+contador_parenteses = 0
+for parenteses in expressao:
+    if parenteses == '(':
+        contador_parenteses += 1
+    elif parenteses == ')':
+        contador_parenteses -= 1
+    if contador_parenteses < 0:
+        break
+if contador_parenteses == 0:
+    print('Expressão válida')
+else:
+    print('Expressão inválida')
