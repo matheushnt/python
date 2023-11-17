29f95de36791a1f96b3fdf59b192b94158da9622
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-11-17 14:23:10 -0300

    teste realizado

---
+++ testes/aula18_b.py
@@ -0,0 +1,6 @@
+galera = [['Matheus', 18], ['Gislane', 19], ['Fabinho', 13]]
+print(galera)   # exibe todo o conteúdo da lista galera
+print(galera[0][0])     # Matheus
+print(galera[1][0])     # Gislane
+print(galera[2][1])     # 13
+print(galera[0:2])      # [['Matheus', 18], ['Gislane', 19]]
