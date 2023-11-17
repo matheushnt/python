65710e1f15f0c91b7195a9911c6219aa6ea9fa29
matheushnt <133993332+matheushnt@users.noreply.github.com>
2023-11-17 18:21:29 -0300

    teste realizado

---
+++ testes/aula18_c.py
@@ -0,0 +1,3 @@
+galera = [['Matheus', 18], ['Gislane', 19], ['Fabinho', 13], ['Joaquim', 20]]   # lista composta
+for pessoa in galera:   # analisa cada pessoa dentro da lista galera
+    print(f'{pessoa[0]} tem {pessoa[1]} anos de idade.')
