d85e29d4237cd743af2d5304427d36eda246cb1d
matheushnt <matheushnt06@gmail.com>
2023-11-22 14:24:54 -0300

    exercício concluído

--- exercicios/ex085.py
+++ exercicios/ex085.py
@@ -1,29 +1,27 @@
+print('= ='*10)
+print(f'{'LISTAS COM PARES E ÍMPARES':^30}')
+print('= ='*10)
 numeros_analisados = [[], []]
 for cont in range(0, 10):
     num = int(input('Digite um número: '))
     if num % 2 == 0:
-        if cont == 0 or num > numeros_analisados[0][-1]:
-            numeros_analisados[0].append(num)
-        else:
-            indice = 0
-            while indice < len(numeros_analisados[0]):
-                if num <= numeros_analisados[0][indice]:
-                    numeros_analisados[0].insert(indice, num)
-                    break
-                indice += 1
+        numeros_analisados[0].append(num)
+        numeros_analisados[0].sort()
     else:
-        if cont == 0 or num > numeros_analisados[1][-1]:
-            numeros_analisados[1].append(num)
-        else:
-            indice = 0
-            while indice < len(numeros_analisados[1]):
-                if num <= numeros_analisados[1][indice]:
-                    numeros_analisados[1].insert(indice, num)
-                    break
-                indice += 1
-print('Os números pares cadastrados foram: ', end=' ')
-for valor in numeros_analisados[0]:
-    print(f'{valor}', end=' ')
-print('\nOs números ímpares cadastrados foram: ', end=' ')
-for valor in numeros_analisados[1]:
-    print(f'{valor}', end=' ')
+        numeros_analisados[1].append(num)
+        numeros_analisados[1].sort()
+    print('-'*30)
+if len(numeros_analisados[0]) == 0:
+    print('Não foi registrado nenhum número par.')
+else:
+    print('Os números pares cadastrados foram: ', end=' ')
+    for valor in numeros_analisados[0]:
+        print(f'{valor}', end=' ')
+print('\n- - - - - - - - - - - - - - - - - - - - - - - - -')
+if len(numeros_analisados[1]) == 0:
+    print('Não foi registrado nenhum número ímpar.')
+else:
+    print('Os números ímpares cadastrados foram: ', end=' ')
+    for valor in numeros_analisados[1]:
+        print(f'{valor}', end=' ')
+print('\n= = = = = = = = = = = = FIM = = = = = = = = = = = =', end='')
