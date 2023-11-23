566122bf5ce71ba57ee8322cc7aba0b1f9458071
matheushnt <matheushnt06@gmail.com>
2023-11-23 12:29:05 -0300

    exercício concluído

--- exercicios/ex088.py
+++ exercicios/ex088.py
@@ -1,12 +1,18 @@
 from random import randint
+print('= ='* 10)
+print(f'{'MEGA SENA':^30}')
+print('= ='* 10)
 jogadas = []
 sorteio = []
 jogos = int(input('Quantas jogadas você fará?\n'))
+print(f'-=-=-=-= SORTEANDO {jogos} JOGOS =-=-=-=-')
 for cont in range(0, jogos):
     inicio = 0
     while inicio < 6:
         sorteio.append(randint(0, 60))
-        jogadas.append(sorteio[:])
-        sorteio.clear()
         inicio += 1
-print(jogadas)
+    jogadas.append(sorteio[:])
+    sorteio.clear()
+for posicao, jogo in enumerate(jogadas):
+    print(f'Jogo {posicao+1}: {jogadas[posicao]}')
+print(f'=-=-=-=-={' < BOA SORTE! > ':^5}=-=-=-=-=')    
