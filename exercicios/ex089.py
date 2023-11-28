67a372d923642db6ea2e288e47c6c9aa91040451
matheushnt <matheushnt06@gmail.com>
2023-11-28 10:09:46 -0300

    exercício concluído

--- exercicios/ex089.py
+++ exercicios/ex089.py
@@ -1,33 +1,37 @@
+print('= =' * 10)
+print(f'{'BOLETIM COM LISTAS COMPOSTAS':^30}')
+print('= =' * 10)
 ficha_estudantil = []
 estudante = []
 while True:
     estudante.append(str(input('Digite seu nome:\n')).strip())
     estudante.append(float(input('Primeira Nota: ')))
     estudante.append(float(input('Segunda Nota: ')))
-    media = (estudante[1] + estudante[2]) / 2
+    media = (estudante[1] + estudante[2]) / 2   # calcula a média de cada estudante
     estudante.append(media)
-    ficha_estudantil.append(estudante[:])
-    estudante.clear()
+    ficha_estudantil.append(estudante[:])   # adiciona uma cópia da lista estudante na lista ficha_estudantil 
+    estudante.clear()   # limpa o conteúdo da lista estudante
     resposta = str(input('Quer continuar? [S/N]\n')).strip()[0]
     while resposta not in 'SsNn':
         resposta = str(input('Quer continuar? [S/N]\n')).strip()[0]
     if resposta in 'Nn':
         break
-print(' -' * 15)
-print(f'{'BOLETIM ESCOLAR':^30}')
-print(' -' * 15)
-print(f'{'N°':<5} {'NOME':<10} {'MÉDIA':>15}')
-
+print('=' * 42)
+print(f'{'BOLETIM ESCOLAR':^42}')
+print('=' * 42)
+print(f'{'N°':<5} {'NOME':<15} {'MÉDIA':>20}')
+print('-' * 42)
 for indice, estudante in enumerate(ficha_estudantil):
-    print(f'{indice:<5} {estudante[0]:<10} {estudante[3]:>15}')
-print('--' * 15)
+    print(f'{indice:<5} {estudante[0]:<15} {estudante[3]:>20}')     # exibe o índice, o nome e a média de cada estudante registrado respectivamente
+    print('-' * 42)
 while True:
     notas = int(input('Mostrar notas de qual estudante? [999 para PARAR]: '))
     if notas == 999:
         break
-    for nota, estudante in enumerate(ficha_estudantil):     # foi encontrado um problema no FOR
-        if notas == nota:
-            print(f'Notas de {estudante[0]} são:', end=' ')
-            for estudante in ficha_estudantil:
-                print(f'{estudante[1:3][nota]}', end=' ')   # meu problema está aqui
-    print('\nEstudante não registrado.', end=' ')       
+    incr = 0    # verifica se o estudante foi registrado
+    for nota, estudante in enumerate(ficha_estudantil):
+        if notas == nota:   # verifica se o número digitado corresponde ao índice do estudante
+            print(f'Notas de {estudante[0]} são: {ficha_estudantil[nota][1:3]}')    # exibe o nome e as notas do estudante
+            incr += 1   # se a condição for atendida, é porque o estudante foi registrado
+    if incr == 0:   # se não houver incrementos, é porque o estudante não foi registrado 
+        print('Estudante não registrado.', end=' ')
