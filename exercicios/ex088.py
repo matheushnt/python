e530e12186616473275802f1401b67774e4e1ed6
matheushnt <matheushnt06@gmail.com>
2023-11-30 11:29:12 -0300

    fix: correção do erro de duplicidade de números

--- exercicios/ex088.py
+++ exercicios/ex088.py
@@ -7,12 +7,17 @@ sorteio = []
 jogos = int(input('Quantas jogadas você fará?\n'))
 print(f'-=-=-=-= SORTEANDO {jogos} JOGOS =-=-=-=-')
 for cont in range(0, jogos):
-    inicio = 0
-    while inicio < 6:
-        sorteio.append(randint(0, 60))
-        inicio += 1
+    cont = 0
+    while True:
+        num = randint(0, 60)
+        if num not in sorteio:
+            sorteio.append(num)
+            cont += 1
+        if cont == 6:
+            break
     jogadas.append(sorteio[:])
     sorteio.clear()
 for posicao, jogo in enumerate(jogadas):
     print(f'Jogo {posicao+1}: {jogadas[posicao]}')
 print(f'=-=-=-=-={' < BOA SORTE! > ':^5}=-=-=-=-=')    
+ 
