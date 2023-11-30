df2e6e8fcf918fb9b21aca88c59f9cb979f8e533
matheushnt <matheushnt06@gmail.com>
2023-11-30 11:33:02 -0300

    docs: explicando as funcionalidades do exercício

--- exercicios/ex088.py
+++ exercicios/ex088.py
@@ -6,16 +6,16 @@ jogadas = []
 sorteio = []
 jogos = int(input('Quantas jogadas você fará?\n'))
 print(f'-=-=-=-= SORTEANDO {jogos} JOGOS =-=-=-=-')
-for cont in range(0, jogos):
+for cont in range(0, jogos):    # laço para gerar a quantidade de jogos pedidos
     cont = 0
-    while True:
+    while True: # laço para gerar 6 números aleatórios diferentes para cada jogo
         num = randint(0, 60)
         if num not in sorteio:
             sorteio.append(num)
             cont += 1
         if cont == 6:
             break
-    jogadas.append(sorteio[:])
+    jogadas.append(sorteio[:])  # adiciona uma cópia de cada jogo na lista jogadas
     sorteio.clear()
 for posicao, jogo in enumerate(jogadas):
     print(f'Jogo {posicao+1}: {jogadas[posicao]}')
