ae2fca82c8dcb5ef302d5e03cfd132dff01b6242
matheushnt <matheushnt06@gmail.com>
2023-11-30 18:26:51 -0300

    teste realizado

---
+++ testes/aula19_b.py
@@ -0,0 +1,7 @@
+""" DECLARANDO UM DICIONÁRIO """
+pessoa = {'nome': 'Joana',
+          'idade': 45,
+          'sexo': 'F'}
+print(pessoa.keys())    # retorna as chaves do dicionário
+print(pessoa.values)    # retorna os valores das chaves do dicionário
+print(pessoa.items())   # retorna o os itens (conjunto chave-valor) do dicionário 
