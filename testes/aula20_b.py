534304b4856379800dcddb40ea17f52ad2a927dd
matheushnt <matheushnt06@gmail.com>
2023-12-19 18:38:31 -0300

    teste realizado

---
+++ testes/aula20_b.py
@@ -0,0 +1,10 @@
+def titulo(txt):    # definindo a função titulo() passando a ela um PARÂMETRO
+    print('-'*30)
+    print(txt)      # é exibido o valor desse parâmetro
+    print('-'*30)
+
+
+""" PROGRAMA PRINCIPAL """
+titulo(f'{'CURSO EM VÍDEO':^30}')   # chamando a função e passando o valor do parâmetro
+titulo(f'{'APRENDENDO FUNÇÕES':^30}')   # chamando a função e passando o valor do parâmetro
+titulo(f'{'MATHEUS HENRIQUE':^30}')   # chamando a função e passando o valor do parâmetro
