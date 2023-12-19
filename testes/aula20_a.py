a72a3b5fd14c9a6c37838781dd7678299d3c0b44
matheushnt <matheushnt06@gmail.com>
2023-12-19 18:21:47 -0300

    teste realizado

---
+++ testes/aula20_a.py
@@ -0,0 +1,10 @@
+def linha():    # definindo a função linha()
+    print('-'*30)
+
+
+""" PROGRAMA PRINCIPAL """
+linha() # chamando a função linha()
+print('     Matheus     ')
+linha() # chamando a função linha()
+print('     Henrique     ')
+linha() # chamando a função linha()
