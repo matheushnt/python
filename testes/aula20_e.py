17fe0d2821ec5da22bc8744d674aa0d83cb132da
matheushnt <matheushnt06@gmail.com>
2023-12-20 07:57:53 -0300

    teste realizado

--- testes/aula20_e.py
+++ testes/aula20_e.py
@@ -1,10 +1,11 @@
 def dobra(lst):
     pos = 0
     while pos < len(lst):
-        lst[pos] *= 2
+        lst[pos] *= 2   # Cada valor percorrido na lista será dobrado
         pos +=1
     print(lst)
 
 
-lista = [2, 5, 9, 6, 3, 4]
-dobra(lista)
+""" PROGRAMA PRINCIPAL """
+lista = [2, 5, 9, 6, 3, 4]  # Lista que servirá como um parâmetro
+dobra(lista)    # lista passada como um parâmetro
