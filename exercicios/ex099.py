1355ed799c7141583b6d6c3cb52edb15aa0dffec
Matheus <matheushnt06@gmail.com>
2023-12-20 15:49:13 -0300

    formatando o exercício

--- exercicios/ex099.py
+++ exercicios/ex099.py
@@ -1,21 +1,22 @@
 from time import sleep
 
-def maior(* num):   # Definindo uma função com desempacotamento
-    print('= ='*15)
+
+def maior(*num):  # Definindo uma função com desempacotamento
+    print("= =" * 15)
     tam = len(num)
-    if tam != 0:    # executa a análise se algum valor foi passado como parâmetro
+    if tam != 0:  # executa a análise se algum valor foi passado como parâmetro
         pos = 0
-        while pos < tam:   # Laço para percorrer a Tupla com os parâmetros
-            if pos == 0 or num[pos] > m:    # Verificação do maior número
-                m = num[pos]  
-            pos += 1  
+        while pos < tam:  # Laço para percorrer a Tupla com os parâmetros
+            if pos == 0 or num[pos] > m:  # Verificação do maior número
+                m = num[pos]
+            pos += 1
         for valor in num:
-            print(valor, end=' ', flush=True)
+            print(valor, end=" ", flush=True)
             sleep(0.3)
-        print(f'=> Foram informados {tam} valores ao todo.')
-        print(f'O maior valor informado foi o {m}')
-    else:   # Não realiza a análise se nenhum valor foi passado como parâmetro
-        print('Impossível análise. Nenhum valor informado.')
+        print(f"=> Foram informados {tam} valores ao todo.")
+        print(f"O maior valor informado foi o {m}")
+    else:  # Não realiza a análise se nenhum valor foi passado como parâmetro
+        print("Impossível análise. Nenhum valor informado.")
 
 
 """ PROGRAMA PRINCIPAL """
