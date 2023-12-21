4458809f1efcd4c5da4def2b35a8196a6ebf78e5
Matheus <matheushnt06@gmail.com>
2023-12-21 16:29:05 -0300

    teste realizado

---
+++ testes/aula21_a.py
@@ -0,0 +1,6 @@
+""" MANEIRAS DE LER A DOC DE UMA FUNÇÃO """
+# É possível acessar a documentação de uma função através do Python Console
+# Utilizando a função help()  EXEMPLO: help(print)
+# Utilizando print(input.__doc__)
+help(print)
+print(input.__doc__)
