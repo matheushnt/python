3176b394ceb333b93e217cf8c0a3688387c2c2cf
Matheus <matheushnt06@gmail.com>
2023-12-25 15:20:19 -0300

    exercício concluído

--- exercicios/ex104.py
+++ exercicios/ex104.py
@@ -1,14 +1,25 @@
 def lnum(msg):  # parâmetro formal
+    """
+    -> Função que lê e valida a entrada de dados
+    :param msg: mensagem da função lnum()
+    :return: retorna um valor numérico
+    """
     cond = False
     valor = 0
     while True:
         global num
-        num = str((input(msg)))
+        # Input que solicita dados do usuário
+        num = input(msg)
+        # Condição para verificar se os dados informados são numéricos
         if num.isnumeric():
+            # Caso seja, o valor é tranformado para inteiro
             valor = int(num)
+            # A Condição se torna VERDADE e o laço se encerrará
             cond = True
+        # Caso os dados informados não sejam numéricos, o laço se repete
         else:
             print("\033[0;031mERRO! Digite um número inteiro válido.\033[m")
+        # Se a condição for VERDADE, o laço é interrompido
         if cond:
             break
     return valor
