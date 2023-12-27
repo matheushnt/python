5968ceb27e4e88b4c99256cd9eedc4f494b4d2ba
matheus <matheushnt06@gmail.com>
2023-12-27 12:12:23 -0300

    módulo moeda criado

---
+++ utilidades/moeda.py
@@ -0,0 +1,34 @@
+def aumentar(n):
+    """
+    -> Função que aumenta em 27% do valor informado
+    :param n: valor informado pelo usuário
+    :return: retorna o valor calculado
+    """
+    return n * 0.27
+
+
+def diminuir(n):
+    """
+    -> Função que diminue em 34% do valor informado
+    :param n: valor informado pelo usuário
+    :return: retorna o valor calculado
+    """
+    return n - (n * 0.34)
+
+
+def metade(n):
+    """
+    -> Função que calcula metade do valor informado
+    :param n: valor informado pelo usuário
+    :return: retorna o valor calculado
+    """
+    return n / 2
+
+
+def dobro(n):
+    """
+    -> Função que calcula o dobro do valor informado
+    :param n: valor informado pelo usuário
+    :return: retorna o valor calculado
+    """
+    return n * 2
