6b67639fbaa48742e589c50da37ed772c2667fc7
matheus <matheushnt06@gmail.com>
2023-12-27 14:15:05 -0300

    feat: adição da função de formatação monetária brasileira

--- exercicios/ex108.py
+++ exercicios/ex108.py
@@ -6,7 +6,7 @@ from utilidades.moeda import *
 
 titulo('formatação monetária brasileira')
 num = float(input('Digite um preço: R$ '))
-print(f'A metade de {num} é {moeda(metade(num))}')
-print(f'O dobro de {num} é {moeda(dobro(num))}')
-print(f'Aumentando em 27%, temos {moeda(aumentar(num))} ')
-print(f'Diminuindo em 34%, temos {moeda(diminuir(num))}')
+print(f'A metade de {num} é {moeda(metade(num), True)}')
+print(f'O dobro de {num} é {moeda(dobro(num), True)}')
+print(f'Aumentando em 27%, temos {moeda(aumentar(num), True)}')
+print(f'Diminuindo em 34%, temos {moeda(diminuir(num), True)}')
