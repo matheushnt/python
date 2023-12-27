f5c879da72f9bd472467cf379c42504240578d60
matheus <matheushnt06@gmail.com>
2023-12-27 14:03:39 -0300

    exercício concluído

---
+++ exercicios/ex108.py
@@ -0,0 +1,12 @@
+# Importando apenas a função de título do Módulo Textos do Pacote Utilidades
+from utilidades.textos import titulo
+# Importando todas as funções do Módulo Moeda do Pacote Utilidades
+from utilidades.moeda import *
+
+
+titulo('formatação monetária brasileira')
+num = float(input('Digite um preço: R$ '))
+print(f'A metade de {num} é {moeda(metade(num))}')
+print(f'O dobro de {num} é {moeda(dobro(num))}')
+print(f'Aumentando em 27%, temos {moeda(aumentar(num))} ')
+print(f'Diminuindo em 34%, temos {moeda(diminuir(num))}')
