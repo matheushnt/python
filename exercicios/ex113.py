f0d8061a483393e110541081a23e5432795489a5
Matheus <matheushnt06@gmail.com>
2024-01-03 20:34:06 -0300

    refactor: exercício refatorado

--- exercicios/ex113.py
+++ exercicios/ex113.py
@@ -1,87 +1,57 @@
-def ler_int(msg):
+def ler_int():
     """
-    -> Função para validar números inteiros
-    :param msg: mensagem para a função input()
-    :return: sem retorno
+    -> Função que valida Números Inteiros.
+    :return: retorna o Número Inteiro digitado.
     """
-    # Tentativa de obter um número inteiro
-    try:
-        num = int(input(msg))
+    # É feita várias tentativas até o usuário informar um Número Inteiro
+    while True:
+        # Tentativa de obter um Número Inteiro
+        try:
+            num = int(input('Digite um Número Inteiro: '))
 
-    # Dispara a Exceção caso o usuário interrompa a execução do programa
-    except KeyboardInterrupt:
-        print('Dados não foram inseridos')
+        # Caso o usuário informe números não inteiros, dispara uma Exceção
+        except (ValueError, TypeError):
+            print('\033[0;31mERRO: os dados informados são inválidos.\033[m')
+            # Interrompe o laço e pula para próxima iteração
+            continue
 
-    # Dispara a Exceção caso os dados sejam de tipagem diferente do pedido
-    except (ValueError, TypeError):
-        print('O valor informado é inválido')
+        # Caso o usuário não digite valores, dispara uma Exceção
+        except KeyboardInterrupt:
+            print('O usuário preferiu não informar valores.')
+            break
 
-        # É realizado várias tentativas até o usuário informar um valor válido
-        while True:
-            try:
-                """ BREVE VALIDAÇÃO DE DADOS """
-                cond = False
-                num = str(input(msg))
-                if num.isnumeric():
-                    cond = True
-                    num = int(num)
-                else:
-                    print('O valor informado é inválido')
-                if cond:
-                    print(f'Você digitou o número inteiro {num}')
-                    break
-
-            # Dispara a Exceção caso o usuário interrompa a execução do programa
-            except KeyboardInterrupt:
-                print('Dados não foram inseridos')
-                # Interrompe o laço
-                break
-    else:
-        print(f'Você digitou o número inteiro {num}')
+        # Caso usuário digite o valor esperado, o programa é encerrado com êxito
+        else:
+            return print(f'Você digitou o Número Inteiro: {num}')
 
 
-def ler_float(msg):
+def ler_float():
     """
-    -> Função para validar números decimais
-    :param msg: mensagem para a função input()
-    :return: sem retorno
+    -> Função que valida Números Reais.
+    :return: retorna o Número Real digitado.
     """
-    # Tentativa de obter um número inteiro
-    try:
-        num = float(input(msg))
+    # É feita várias tentativas até o usuário informar um Número Real
+    while True:
+        # Tentativa de obter um Número Inteiro
+        try:
+            num = float(input('Digite um Número Real: '))
 
-    # Dispara a Exceção caso o usuário interrompa a execução do programa
-    except KeyboardInterrupt:
-        print('Dados não foram inseridos')
+        # Caso o usuário informe números não reais, dispara uma Exceção
+        except (ValueError, TypeError):
+            print('\033[0;31mERRO: os dados informados são inválidos.\033[m')
+            # Interrompe o laço e pula para próxima iteração
+            continue
 
-    # Dispara a Exceção caso os dados sejam de tipagem diferente do pedido
-    except (ValueError, TypeError):
-        print('O valor informado é inválido')
+        # Caso o usuário não digite valores, dispara uma Exceção
+        except KeyboardInterrupt:
+            print('O usuário preferiu não informar valores.')
+            break
 
-        # É realizado várias tentativas até o usuário informar um valor válido
-        while True:
-            try:
-                """ BREVE VALIDAÇÃO DE DADOS """
-                cond = False
-                num = str(input(msg))
-                if num.isnumeric():
-                    cond = True
-                    num = float(num)
-                else:
-                    print('O valor informado é inválido')
-                if cond:
-                    print(f'Você digitou o número decimal {num:.2f}')
-                    break
-
-            # Dispara a Exceção caso o usuário interrompa a execução do programa
-            except KeyboardInterrupt:
-                print('Dados não foram informados')
-                # Interrompe o laço
-                break
-    else:
-        print(f'Você digitou o número decimal {num:.2f}')
+        # Caso usuário digite o valor esperado, o programa é encerrado com êxito
+        else:
+            return print(f'Você digitou o Número Real: {num:.2f}')
 
 
 # Programa principal
-ler_int('Digite um Inteiro: ')
-ler_float('Digite um Decimal: ')
+ler_int()
+ler_float()
