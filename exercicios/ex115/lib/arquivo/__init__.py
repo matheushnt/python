a9972a46c34bc9eed022caa000f1a9c0e05e8ac5
Matheus <matheushnt06@gmail.com>
2024-01-04 17:31:59 -0300

    feat: criação da função cadastrar()

--- exercicios/ex115/lib/arquivo/__init__.py
+++ exercicios/ex115/lib/arquivo/__init__.py
@@ -21,11 +21,31 @@ def criar_arq(nome):
         print(f'Arquivo {nome} criado com sucesso')
 
 
+# noinspection PyGlobalUndefined
 def ler_arq(nome):
+    # noinspection PyGlobalUndefined
+    global file
     try:
-        txt = open(nome, 'rt')
+        file = open(nome, 'rt')
     except Exception as erro:
         print(f'Não é possível ler arquivo. Motivo: {erro.__class__}')
     else:
         cabecalho('PESSOAS CADASTRADAS')
-        print(txt.read())
+        print(file.read())
+    finally:
+        file.close()
+
+
+def cadastrar(arq, nm='<desconhecido>', idd=0):
+    try:
+        txt = open(arq, 'at')
+    except Exception as erro:
+        print(f'Não foi possível abrir o Arquivo. Motivo: {erro.__class__}')
+    else:
+        try:
+            txt.write(f'{nm:.<30}{idd:>3} anos\n')
+        except Exception as erro:
+            print(f'Houve um ERRO no momento de inserir os dados. Motivo: {erro.__class__}')
+        else:
+            print(f'Novo registro de {nm} foi adicionado.')
+            txt.close()
