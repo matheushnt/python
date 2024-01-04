f6e11d82be0528598ac05f432fd1442a7bffa626
Matheus <matheushnt06@gmail.com>
2024-01-04 14:16:22 -0300

    documentando as funções linha() e cabecalho()

--- exercicios/ex115/lib/interface/__init__.py
+++ exercicios/ex115/lib/interface/__init__.py
@@ -1,8 +1,18 @@
 def linha(tam=42):
+    """
+    -> Função que cria uma linha horizontal para demarcação de cabeçalho.
+    :param tam: Tamanho da linha.
+    :return: Retorna a linha.
+    """
     return '-' * tam
 
 
 def cabecalho(txt):
+    """
+    -> Função para criar um cabeçalho.
+    :param txt: Mensagem do Cabeçalho
+    :return: sem retorno
+    """
     linha()
     print(txt.center())
     linha()
